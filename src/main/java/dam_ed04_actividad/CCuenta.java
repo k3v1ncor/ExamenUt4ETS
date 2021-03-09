655d965c412b7fa1001588224cf6b70d077fee49
1cfgs <1cfgs@A6-PC10>
2021-03-09 08:24:55 +0000

    3.Encapsula los cuatro atributos de la clase Ccuenta.

--- src/main/java/dam_ed04_actividad/CCuenta.java
+++ src/main/java/dam_ed04_actividad/CCuenta.java
@@ -8,6 +8,34 @@ package dam_ed04_actividad;
 
 public class CCuenta {
 
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
 
     private String nombre;
     private String cuenta;
@@ -39,7 +67,7 @@ public class CCuenta {
 
     public double estado()
     {
-        return saldo;
+        return getSaldo();
     }
 
 
@@ -47,7 +75,7 @@ public class CCuenta {
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);
     }
 
     public void retirar(double cantidad) throws Exception
@@ -56,13 +84,13 @@ public class CCuenta {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
 
 
     public String obtenerCuenta()
     {
-        return cuenta;
+        return getCuenta();
     }
 
 
